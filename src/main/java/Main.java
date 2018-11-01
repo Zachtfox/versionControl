11dec7895aed832a66bb411538e4816d97d8e485
Zach <zfox@students.mchenry.edu>
2018-11-01 10:33:25 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Zach Fox. I work at the Jewel-Osco in McHenry.");
+        System.out.println("I will be turning 21 on Christmas Eve this year.");
+        System.out.println(" I have 2 pets at home, a dog and a cat. I consider myself a dog person, but that might be, in part, because all the cats I have had were evil.");
+        
+    }
+}
